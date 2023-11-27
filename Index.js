c9b14d4740af368f94f3ab547f2165d1190d0b29
babydetroyer3k <145839067+babydetroyer3k@users.noreply.github.com>
2023-11-27 11:41:18 +0100

    Playerdead Check

--- Index.js
+++ Index.js
@@ -4,4 +4,14 @@ var Enemy = 0;
 var Player = 0;
 var HP = 100;
 var speed = 100;
+playerdead = false
 
+function isplayerdead() {
+if (HP == 0) {
+    playerdead = true;
+}
+}
+
+function shoot() {
+    
+}
