112bf173e7ae5deadb1abdf02c5715000c88c089
olex-dev-dk <77013037+olex-dev-dk@users.noreply.github.com>
2023-11-27 11:56:25 +0100

    !

--- Index.js
+++ Index.js
@@ -1,14 +1,9 @@
+// tissemand
+
 var Enemy = 0;
 var Player = 0;
 var HP = 100;
 var speed = 100;
-playerdead = false
-
-function isplayerdead() {
-if (HP == 0) {
-    playerdead = true;
-}
-}
 
 function Shoot() {
     
