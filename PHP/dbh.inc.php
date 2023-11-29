fb9cd6452d95e8d595bf3dec2fde8db57475870b
babydetroyer3k <145839067+babydetroyer3k@users.noreply.github.com>
2023-11-29 10:15:04 +0100

    PHP

---
+++ PHP/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=leaderboard";
+$dbusername = "root";
+$dbpassword = "";
+
+$pdo = new PDO($dsn, $dbusername, $dbpassword);
+
+?>
---
+++ PHP/formhandler.inc.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $highscore = $_POST["highscore"];
+
+    try {
+        require_once "dbh.inc.php";
+        $query = "INSERT INTO users (username, highscore) VALUES 
+        (?, ?, ?);";
+
+    $stmt = $pdo->prepare($query);
+
+    $stmt->execute([$username, $highscore]);
+
+    $pdo = null;
+    $stmt = null;
+
+    header("Location: index.php");
+
+    die();
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+} else {
+    header("Location: index.php");
+}
+?>
